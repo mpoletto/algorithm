3ec23f3c48847874a831f53e7fd11aac61ba8aa0
Marcos <mpoletto@gmail.com>
2024-01-28 16:23:11 -0300

    Code in Python3 included.

---
+++ triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def triangle(x):
+    a = [[1]]
+    for i in range(1, x+1):
+        tmp = []
+        for j in range(i):
+            if j == 0:
+                tl = 0 + a[i-1][j]
+                tmp.append(tl)
+            elif j < len(a[i-1]): 
+                tm = a[i-1][j-1] + a[i-1][j]
+                tmp.append(tm)
+            
+            if j == len(a[i-1])-1:
+                tr = a[i-1][j] + 0
+                tmp.append(tr)
+        a.append(tmp)
+
+    return a
+
+
+print(triangle(10))
